97edfa051a08dbdd04c6e086b74a9f302e7f52b4
CattonNyan <kkt991208@gmail.com>
2026-08-12 17:36:31 +0900

    SO1

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProductByid/GetProductByidEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace CatalogAPI.Products.GetProductByid
+{
+    public class GetProductByidEndpoint
+    {
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/Products/GetProductByid/GetProductByidHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace CatalogAPI.Products.GetProductByid;
+
+public record GetProductByidQuery(Guid Id) : IQuery<GetProductByidResult>;
+
+public record GetProductByidResult(Product Product);
+
+
+
+internal class GetProductByidQueryHandler
+    (IDocumentSession session, ILogger<GetProductByidQueryHandler> logger)
+    : IQueryHandler<GetProductByidQuery, GetProductByidResult>
+{
+    public async Task<GetProductByidResult> Handle(GetProductByidQuery query, CancellationToken cancellationToken)
+    {
+        
+        logger.LogInformation("GetProductByidQueryHandler.Handle called with {@Query}", query);
+        
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if(product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByidResult(product);
+
+    }
+}
