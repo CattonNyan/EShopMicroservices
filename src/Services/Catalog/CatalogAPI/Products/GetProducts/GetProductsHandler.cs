51e059590611c995f05e9765fae474dd675d6e95
kmkim <kmkim@ksystem.co.kr>
2026-08-12 17:22:31 +0900

    add something new

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace CatalogAPI.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(
+    IDocumentSession session,
+    ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(
+        GetProductsQuery query,
+        CancellationToken cancellation)
+    {
+        logger.LogInformation(
+            "GetProductsQueryHandler.Handle called with {@Query}", query);
+
+        var products = await session
+            .Query<Product>()
+            .ToListAsync(cancellation);
+
+        return new GetProductsResult(products);
+    }
+}
--- src/Services/Catalog/CatalogAPI/Products/GreatProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/GreatProduct/CreateProductHandler.cs
@@ -1,12 +1,8 @@
-﻿using BuildingBlocks.CQRS;
-using CatalogAPI.Models;
-using MediatR;
-
-namespace CatalogAPI.Products.GreatProduct;
+﻿namespace CatalogAPI.Products.GreatProduct;
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid ID);
-internal class CreateProductCommandHandler 
+internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -24,9 +20,13 @@ internal class CreateProductCommandHandler
             Price = command.Price
         };
 
+        //TODO
         //save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
         // return result
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 
 }
