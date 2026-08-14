418049df8ff9595ef031965575f8739fad8f3c69
CattonNyan <kkt991208@gmail.com>
2026-08-14 14:57:43 +0900

    Build Models and Data

--- src/Services/src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
+++ src/Services/src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -2,11 +2,11 @@
 
 public class ShoppingCartItem
 {
-    public int Quntity { get; set; } = default!;
+    public int Quantity { get; set; } = default!;
 
     public string Color { get; set; } = default!;
 
-    public string Price { get; set; } = default!;
+    public decimal Price { get; set; } = default!;
 
     public Guid ProductId { get; set; } = default!;
 
--- src/Services/src/Services/Basket/Basket.API/Program.cs
+++ src/Services/src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,22 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var assembly = typeof(Program).Assembly;
+
 
 builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
