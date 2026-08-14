3c1efe98b89fd578dcf1583beb3bcf35d62458c0
CattonNyan <kkt991208@gmail.com>
2026-08-14 14:20:30 +0900

    Build Basket Module

---
+++ src/Services/src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCartItem
+{
+    public int Quntity { get; set; } = default!;
+
+    public string Color { get; set; } = default!;
+
+    public string Price { get; set; } = default!;
+
+    public Guid ProductId { get; set; } = default!;
+
+    public string ProductName { get; set; } = default!;
+}
--- src/Services/src/Services/Basket/Basket.API/Program.cs
+++ src/Services/src/Services/Basket/Basket.API/Program.cs
@@ -2,8 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCarter();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.MapCarter();
 
 app.Run();
