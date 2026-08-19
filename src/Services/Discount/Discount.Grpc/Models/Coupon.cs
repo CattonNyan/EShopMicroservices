e657eac5b984d5bfa3d98b32e41b551f1767fc64
CattonNyan <kkt991208@gmail.com>
2026-08-19 11:13:56 +0900

    Building DB

--- src/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public string ProductName { get; set; } = default;
+        public string ProductName { get; set; } = default!;
 
-        public string Description { get; set; } = default;
+        public string Description { get; set; } = default!;
 
         public int Amount { get; set; }
     }
